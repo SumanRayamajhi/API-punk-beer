8b06057af0198d2f4de384d25c495d7723064f80
Suman Rayamajhi <meh_sams@yahoo.com>
2022-02-06 01:08:10 +0100

    deploy

--- src/Components/BeerDescription/BeerDescription.js
+++ src/Components/BeerDescription/BeerDescription.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./BeerDescription.css";
 
 function BeerDescription({ beer, onFavouriteAdd, onFavouriteRemove }) {
-  return <div></div>;
+  return <div>To do</div>;
 }
 
 export default BeerDescription;
